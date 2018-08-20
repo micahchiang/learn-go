92f5c7392ab507f3c60d052dce5f32855c024bd8
Micah Chiang <micahkchiang@gmail.com>
2018-08-20 13:55:06 -0700

    api call func

--- src/github.com/micahchiang/go-get-weather/weather.go
+++ src/github.com/micahchiang/go-get-weather/weather.go
@@ -3,8 +3,18 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
+
+func buildURL(city string) string {
+	baseURL := "https://api.openweathermap.org/data/2.5/forecast?q="
+	appID := "&appid=634ab40d1fd51482f9e8d2891c5e45a0"
+	units := "&metric"
+	url := baseURL + city + units + appID
+	return url
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -13,4 +23,12 @@ func main() {
 	scanner.Scan()
 	city := scanner.Text()
 	fmt.Println("Retrieving the weather for", city)
+	url := buildURL(city)
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println("An error occurred: ", err)
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	fmt.Println("finished", body)
 }
