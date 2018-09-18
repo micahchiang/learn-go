f0e87b0895aad73e429607e9628d824c64444ac7
Micah Chiang <micahkchiang@gmail.com>
2018-09-17 17:18:48 -0700

    initial commit for text adventure

---
+++ src/github.com/micahchiang/textadventure/textadventure.go
@@ -0,0 +1,31 @@
+package main
+
+type choices struct {
+	cmd         string
+	description string
+	nextNode    *storyNode
+	nextChoice  *choices
+}
+
+type storyNode struct {
+	text    string
+	choices *choices
+}
+
+func (node *storyNode) addChoice(cmd string, description string, nextNode *storyNode) {
+	choice := &choices{cmd, description, nextNode, nil}
+
+	if node.choices == nil {
+		node.choices = choice
+	} else {
+		currentChoice := node.choices
+		for currentChoice.nextChoice != nil {
+			currentChoice = currentChoice.nextChoice
+		}
+		currentChoice.nextChoice = choice
+	}
+}
+
+func main() {
+
+}
